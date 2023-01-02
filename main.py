55ec64d738bcf33b6a9eaa27a7c9dbe6687ac1ab
Zach <fireknight95@gmail.com>
2023-01-02 15:15:59 -0500

    sql

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ def index():
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
+@app.route('/data')
+def index():
+    connection = pymysql.connect( host='containers-us-west-32.railway.app', user='root', passwd='Jyfcd452Xe3tmMsFLYDY', port=5522, db='railway' )
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `name` FROM `marked_systems` WHERE `id`=%s"
+        cursor.execute(sql, ('1',))
+        result = cursor.fetchone()
+        print(result)
+
+
+
+    response = jsonify({"data": result})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
